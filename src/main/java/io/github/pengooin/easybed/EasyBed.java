75e16715af51d5e85adff0a7d85d1ba76a2aa6db
pengooin <49123800+pengooin@users.noreply.github.com>
2020-07-08 15:33:47 -0500

    Update EasyBed.java
    Removed extraneous import.

--- src/main/java/io/github/pengooin/easybed/EasyBed.java
+++ src/main/java/io/github/pengooin/easybed/EasyBed.java
@@ -6,7 +6,6 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.world.WorldEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
